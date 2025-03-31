c1b39938bed2877eb4badf4be19c557e1b3f0e67
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-03-31 09:59:04 +0300

    исправлены ошибки выявленные выявленные Flake8

--- homework/svetlana_eremeeva/pythonbasic.py
+++ homework/svetlana_eremeeva/pythonbasic.py
@@ -1,68 +1,41 @@
 import math
 
-#1
-
-#разность
-
+# 1
+# Разность
 a = 5
 b = 4
+print(a - b)
 
-print(a-b)
-
-
-#сумма
-
+# Сумма
 a = 3
 b = 4
+print(a + b)
 
-print(a+b)
-
-#произведение
-
+# Произведение
 a = 2
 b = 3
+print(a * b)
 
-print(a*b)
-
-
-
-
-#2
-
+# 2
 x = 2
 y = 3
+print(2 - 3 / 1 + 2 * 3)
 
-print(2-3/1+2*3)
-
-
-
-
-#3
-
-#среднее геометрическое
-
-
+# 3
+# Среднее геометрическое
 a = 4
 c = 6
 average_geometric = math.sqrt(a * c)
-
 print(average_geometric)
 
-#среднее арифметическое
-
+# Среднее арифметическое
 f = 15
 h = 6
+print((f + h) / 2)
 
-print((f+h)/2)
-
-
-
-#4
-
+# 4
 a = 3
 b = 4
-
 hypotenuse = math.sqrt(a**2 + b**2)
-
-print((a*b)/2) #площадь
-print(hypotenuse) #гипотенуза
+print((a * b) / 2)  # площадь
+print(hypotenuse)   # гипотенуза
