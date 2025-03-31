3a92f62ebdf4c41d1f0b4e53cae3b111b2e8284e
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-03-31 09:49:49 +0300

    исправлено на правильный формат файла

---
+++ homework/svetlana_eremeeva/pythonbasic.py
@@ -0,0 +1,68 @@
+import math
+
+#1
+
+#разность
+
+a = 5
+b = 4
+
+print(a-b)
+
+
+#сумма
+
+a = 3
+b = 4
+
+print(a+b)
+
+#произведение
+
+a = 2
+b = 3
+
+print(a*b)
+
+
+
+
+#2
+
+x = 2
+y = 3
+
+print(2-3/1+2*3)
+
+
+
+
+#3
+
+#среднее геометрическое
+
+
+a = 4
+c = 6
+average_geometric = math.sqrt(a * c)
+
+print(average_geometric)
+
+#среднее арифметическое
+
+f = 15
+h = 6
+
+print((f+h)/2)
+
+
+
+#4
+
+a = 3
+b = 4
+
+hypotenuse = math.sqrt(a**2 + b**2)
+
+print((a*b)/2) #площадь
+print(hypotenuse) #гипотенуза
