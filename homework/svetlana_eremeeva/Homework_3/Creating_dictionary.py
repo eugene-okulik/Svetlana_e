d3c31f71d999fb8c50571a50151de6ad03e16b2b
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-04-03 19:51:32 +0300

    Внесение отсутствующих заданий в list и dict

--- homework/svetlana_eremeeva/Homework_3/Creating_dictionary.py
+++ homework/svetlana_eremeeva/Homework_3/Creating_dictionary.py
@@ -2,27 +2,41 @@ my_dict = {
     "tuple": (1, 2, 3, 4, 5),
     "list": ["Амбросов И.А.", "Иванов А.А.", "Крылов И.И", "Красовская Д.О", "Тритонова О.Ю"],
     "dict": {
-        "color": "green",
-        "fruit": "orange",
-        "sky": "blue",
-        "sky": "cloud",
-        "flower": "chamomile",
-        "apple": "Red"
+    "color": "green",
+    "fruit": "orange",
+    "sky": "blue",
+    "flower": "chamomile",
+    "apple": "Red"
     },
     "set": {1.1, 2.2, 3.3, 4.4, 5.5}
 }
 
+#"tuple"
 # Вывод последнего элемента из кортежа
 print(my_dict["tuple"][-1])
 
-# Добавление нового элемента в список
+
+#"list"
+# Добавление нового элемента в список 
 my_dict["list"].append("Королев А.А.")
 print(my_dict["list"])
 
+# удалите второй элемент списка
+poped = my_dict["list"].pop(1)
+print(my_dict["list"])
+
+
+#"dict"
 # Удаление ключа "fruit" из словаря
 popped = my_dict["dict"].pop("fruit")
 print(my_dict["dict"])
 
+# Добавление  элемента с ключом и любым значением:
+my_dict["dict"][("i am a tuple",)] = "And what have you achieved"
+print(my_dict["dict"])
+
+
+#set
 # Добавление нового элемента в множество
 my_dict["set"].add(6.6)
 
