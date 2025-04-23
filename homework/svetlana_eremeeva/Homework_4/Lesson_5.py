e6f10a9a4e222a7cc3923c54b53ece4948b584ca
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-04-23 07:55:43 +0300

    правки к заданию_2

--- homework/svetlana_eremeeva/Homework_4/Lesson_5.py
+++ homework/svetlana_eremeeva/Homework_4/Lesson_5.py
@@ -6,16 +6,20 @@ name, last_name, city, phone, country = person
 
 print(name, last_name, city, phone, country)
 
-
-# Задание_2 Срез
+#Задание_2_срез
 
 text_a = "результат операции: 42"
 text_b = "результат операции: 514"
 text_c = "результат работы программы: 9"
 
-number_a = int(text_a.split(":")[1].strip()) + 10
-number_b = int(text_b.split(":")[1].strip()) + 10
-number_c = int(text_c.split(":")[1].strip()) + 10
+index_a = text_a.index(":") + 2
+number_a = int(text_a[index_a:]) + 10
+
+index_b = text_b.index(":") + 2
+number_b = int(text_b[index_b:]) + 10
+
+index_c = text_c.index(":") + 2
+number_c = int(text_c[index_c:]) + 10
 
 sum_result = number_a + number_b + number_c
 
