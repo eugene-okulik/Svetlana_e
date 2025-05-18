f4dac69f5d7c61e8b2f5ebc27739700f516d896c
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-05-18 19:31:18 +0300

    правки, линт

--- Homework_5/One.py
+++ Homework_5/One.py
@@ -4,18 +4,15 @@ words = text.split()
 new_words = []
 
 for word in words:
-    
-    if word[-1] == '.' or word[-1] == ',':
+    if word[-1] in {'.', ','}:
         punctuation = word[-1]
         clean_word = word[:-1]
     else:
         punctuation = ""
         clean_word = word
 
-    
     new_word = clean_word + "ing" + punctuation
     new_words.append(new_word)
-
 
 new_text = " ".join(new_words)
 print(new_text)
--- Homework_5/Two.py
+++ Homework_5/Two.py
@@ -1,7 +1,6 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
-        print("FuzzBuzz")
-        
+        print("FuzzBuzz")  
     elif number % 3 == 0:
         print("Fuzz")
     elif number % 5 == 0:
