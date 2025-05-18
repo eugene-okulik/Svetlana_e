c8da7eef909db5e7c2d878c7eaaeefbe0924ff53
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-05-18 19:20:44 +0300

    выполнение задач к lesson_6

---
+++ Homework_5/One.py
@@ -0,0 +1,21 @@
+text = "Etiam tincidunt neque erat, quis molestie enim imperdiet vel. Integer urna nisl, facilisis vitae semper at, dignissim vitae libero"
+
+words = text.split()
+new_words = []
+
+for word in words:
+    
+    if word[-1] == '.' or word[-1] == ',':
+        punctuation = word[-1]
+        clean_word = word[:-1]
+    else:
+        punctuation = ""
+        clean_word = word
+
+    
+    new_word = clean_word + "ing" + punctuation
+    new_words.append(new_word)
+
+
+new_text = " ".join(new_words)
+print(new_text)
