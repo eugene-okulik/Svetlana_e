14ee30553f467aaba73d085d516ecaeb92565b76
Scorificator <sv1tlana.alexandrovna@yandex.ru>
2025-06-15 17:57:31 +0300

    Правка линт E226 — missing whitespace around arithmetic operator

--- homework/svetlana_eremeeva/Honework_6/Two.py
+++ homework/svetlana_eremeeva/Honework_6/Two.py
@@ -3,10 +3,10 @@ words = {'I': 3, 'love': 5, 'Python': 1, '!': 50}
 
 for key, value in words.items():
     if key == 'I':
-        print(key*3)
+        print(key * 3)
     elif key == 'love':
-        print(key*5)
+        print(key * 5)
     elif key == 'Python':
-        print(key*1)
+        print(key * 1)
     elif key == '!':
-        print(key*50)
+        print(key * 50)
